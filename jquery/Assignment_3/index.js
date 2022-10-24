4a06f29fa1139ec24a4a2df005953dced5f6d566
MATI UL REHMAN <iammatiz00@gmail.com>
2022-10-24 22:11:04 +0500

    doing

---
+++ jquery/Assignment_3/index.js
@@ -0,0 +1,23 @@
+$(document).ready(function() {
+    $("#hide-btn").click(hide);
+    $("#change-btn").click(function() {
+        $("#box").css("background-color", "green");
+    });
+    $("#refresh").click(function() {
+        location.reload();
+    });
+    $("#fade-in").click(function() {
+        $("#div1").fadeIn();
+        $("#div2").fadeIn("slow");
+        $("#div3").fadeIn(3000);
+    });
+    $("#fade-out").click(function() {
+        $("#div1").fadeOut();
+        $("#div2").fadeOut("slow");
+        $("#div3").fadeOut(3000);
+    });
+});
+
+function hide() {
+    $("div").hide();
+}
