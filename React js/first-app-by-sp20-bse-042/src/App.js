e961edb41a3517af3626b1c4b46d74f55433ae3f
MATI UL REHMAN <iammatiz00@gmail.com>
2022-10-30 16:03:26 +0500

    react bootstrap added

--- React js/first-app-by-sp20-bse-042/src/App.js
+++ React js/first-app-by-sp20-bse-042/src/App.js
@@ -1,9 +1,15 @@
 import "./App.css";
+import "bootstrap/dist/css/bootstrap.css";
+import { Button } from "react-bootstrap";
+import Alert from "react-bootstrap/Alert";
 
 function App() {
   return (
-    <div className="App">
-      <h1>MATI UL REHMAN</h1>
+    <div className="container">
+      <h1> MATI UL REHMAN </h1>
+      <button className="btn btn-primary">Check</button>
+      <Button variant="danger">Hello</Button>
+      <Alert variant="success">Pakistan won</Alert>
     </div>
   );
 }
--- React js/first-app-by-sp20-bse-042/src/Header.js
+++ React js/first-app-by-sp20-bse-042/src/Header.js
@@ -4,6 +4,7 @@ function Header() {
   return (
     <div className="App">
       <h1>Its working!</h1>
+      <img src="/logo512.png" alt="logo" />
     </div>
   );
 }
