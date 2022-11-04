d33fdf529d0c79c1b0b4c80f77b335d6fe0bbe6b
MATI UL REHMAN <iammatiz00@gmail.com>
2022-11-05 01:17:46 +0500

    use stated added

--- React js/first-app-by-sp20-bse-042/src/App.js
+++ React js/first-app-by-sp20-bse-042/src/App.js
@@ -4,24 +4,25 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import data from "./data.json";
 
 
-function App() {
-  return (
-    <div className="App">
-      {" "}
-      {data.map((record) => {
-        return (
-          <div className="col md-4">
-            <Products
-              title={record.title}
-              price={record.price}
-              type={record.type}
-              img={record.img}
-            />{" "}
-          </div>
-        );
-      })}{" "}
-    </div>
-  );
-}
+
+ function App() {
+   return (
+     <div className="App">
+       {" "}
+       {data.map((record) => {
+         return (
+           <div className="col md-4">
+             <Products
+               title={record.title}
+               price={record.price}
+               type={record.type}
+               img={record.img}
+             />{" "}
+           </div>
+         );
+       })}{" "}
+     </div>
+   );
+ }
 
 export default App;
--- React js/first-app-by-sp20-bse-042/src/Components/Products.jsx
+++ React js/first-app-by-sp20-bse-042/src/Components/Products.jsx
@@ -1,13 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Products.css";
+
 const Products = (props) => {
   var title = props.title;
   var price = props.price;
   var info = props.type;
   var priceClass = parseInt(price) <= 10 ? "red" : "green";
+  const [count, setCount] = useState(0);
   const addToCard = () => {
     alert(title + " added to cart");
+    setCount(count + 1);
   };
+
   return (
     <div className="product">
       <h1>Name:{title}</h1>
@@ -18,9 +22,11 @@ const Products = (props) => {
       <h5>Type:{info}</h5>
       <button className="btn btn-success" id="btn" onClick={addToCard}>
         {" "}
-        Add to Cart{" "}
+        Add to Cart
       </button>
-      <hr />
+      <br />
+      <h4>Total Items Added:</h4>
+      <h5>{count}</h5> <hr />
     </div>
   );
 };
