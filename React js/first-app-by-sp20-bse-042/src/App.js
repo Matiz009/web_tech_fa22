1c381710b552ac8f6549c42012849f02b9cd3370
MATI UL REHMAN <iammatiz00@gmail.com>
2022-11-05 01:28:07 +0500

    more features added

--- React js/first-app-by-sp20-bse-042/src/App.js
+++ React js/first-app-by-sp20-bse-042/src/App.js
@@ -2,27 +2,35 @@ import Products from './components/Products';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import data from "./data.json";
+import React, { useState } from "react";
 
-
-
- function App() {
-   return (
-     <div className="App">
-       {" "}
-       {data.map((record) => {
-         return (
-           <div className="col md-4">
-             <Products
-               title={record.title}
-               price={record.price}
-               type={record.type}
-               img={record.img}
-             />{" "}
-           </div>
-         );
-       })}{" "}
-     </div>
-   );
- }
+function App() {
+  const [count, setCount] = useState(0);
+  const increaseLikes = () => {
+    setCount(count + 1);
+  };
+  return (
+    <div className="App">
+      {" "}
+      {data.map((record) => {
+        return (
+          <div className="col md-4">
+            <Products
+              title={record.title}
+              price={record.price}
+              type={record.type}
+              img={record.img}
+            />{" "}
+          </div>
+        );
+      })}{" "}
+      <button onClick={increaseLikes} className="btn btn-dark">
+        Give us 👍
+      </button>
+      <br />
+      Total Likes:<h1>{count}</h1>
+    </div>
+  );
+}
 
 export default App;
