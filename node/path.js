bd979ed6e63ba9ec5deaf8531cd89a848b4d25ba
MATI UL REHMAN <iammatiz00@gmail.com>
2022-12-09 03:11:07 +0500

    path

---
+++ node/path.js
@@ -0,0 +1,9 @@
+const path = require("path");
+//description of these functions can be accessed through the
+//following link https://nodejs.org/api/path.html
+console.clear();
+console.log(path.dirname("D:/web dev/web/node"));
+console.log(path.extname("extension:" + "D:/web dev/web/node/path.js"));
+console.log(path.basename("D:/web dev/web/node"));
+console.log(path.parse("D:/web dev/web/node"));
+console.log(path.resolve("D:/web dev/web/node"));
