8f01597b8215cb745c533db6912c8b1ff040dc2c
MATI UL REHMAN <iammatiz00@gmail.com>
2022-12-18 00:24:49 +0500

    crud completed

--- node/express/connectivity.js
+++ node/express/connectivity.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
-const { CreateUser, getUsers, delUsers } = require("./users");
+const { CreateUser, getUsers, delUsers, UpdateUser } = require("./users");
 mongoose.set("strictQuery", false);
 mongoose
     .connect("mongodb://localhost:27017/sample", {
@@ -14,7 +14,15 @@ mongoose
         //console.log(user);
         let allUsers = await getUsers(); //viewing records
         console.log(allUsers);
-        console.log(await delUsers("639e056e265e40cf2fa00c6a"));
+        //console.log(await delUsers("639e056e265e40cf2fa00c6a"));
+        console.log(
+            await UpdateUser(
+                "639e0fa64f5a6c506fc1f2a6",
+                3,
+                "ahmad@gmail.com",
+                "6bdbhcwu@"
+            )
+        );
     })
     .catch((error) => {
         console.log(error);
--- node/express/users.js
+++ node/express/users.js
@@ -1,5 +1,6 @@
 const userModel = require("./models/sams");
 const CreateUser = async(id, email, password) => {
+    //create
     console.log("Create User");
     let user = new userModel();
     user.id = id;
@@ -9,13 +10,27 @@ const CreateUser = async(id, email, password) => {
     return user;
 };
 const getUsers = async() => {
+    //view
     let users = await userModel.find();
     return users;
 };
 const delUsers = async(_id) => {
+    //delete
     let user = await userModel.findByIdAndDelete(_id);
+    return user;
+};
+
+const UpdateUser = async(_id, id, email, password) => {
+    //create
+    console.log("Create User");
+    let user = await userModel.findById(_id);
+    user.id = id;
+    user.email = email;
+    user.password = password;
+    await user.save();
     return user;
 };
 module.exports.CreateUser = CreateUser;
 module.exports.getUsers = getUsers;
 module.exports.delUsers = delUsers;
+module.exports.UpdateUser = UpdateUser;
