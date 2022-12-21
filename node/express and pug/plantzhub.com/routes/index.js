160faf407fa64747dda1a29b72a4b529d9768f7e
MATI UL REHMAN <iammatiz00@gmail.com>
2022-12-22 02:49:51 +0500

    pug basics

--- node/express and pug/plantzhub.com/routes/index.js
+++ node/express and pug/plantzhub.com/routes/index.js
@@ -4,7 +4,14 @@ var router = express.Router();
 /* GET home page. */
 router.get("/", function(req, res, next) {
     //res.render('index', { title: 'Express' });
-    res.send("<h2>Pakistan Zindabad</h2>");
+    //res.send("<h2>Pakistan Zindabad</h2>");
+    res.render("products");
+});
+
+router.get("/contactus", function(req, res, next) {
+    //res.render('index', { title: 'Express' });
+    //res.send("<h2>Pakistan Zindabad</h2>");
+    res.render("contactus");
 });
 
 module.exports = router;
