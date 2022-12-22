ea55e68bc2333802828d8ec4d17fdc9dc21be208
MATI UL REHMAN <iammatiz00@gmail.com>
2022-12-22 05:57:12 +0500

    basic settings

--- node/express and pug/plantzhub.com/routes/products.js
+++ node/express and pug/plantzhub.com/routes/products.js
@@ -7,7 +7,19 @@ router.get("/", async function(req, res, next) {
     let products = await Product.find();
     console.log(products);
     //res.send(products);
-    res.render("products", { title: "Products In DB", products });
+    res.render("products", { products });
 });
+
+router.get("/add", async function(req, res, next) {
+    res.render("products/add");
+});
+router.get("/delete", async function(req, res, next) {
+    res.render("products/delete");
+});
+router.get("/edit", async function(req, res, next) {
+    res.render("products/edit");
+});
+
+
 
 module.exports = router;
