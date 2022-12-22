ac2c7ffb00d1b4d342b861903cda9d0cff4519ba
MATI UL REHMAN <iammatiz00@gmail.com>
2022-12-22 06:53:25 +0500

    crud completed

--- node/express and pug/plantzhub.com/routes/products.js
+++ node/express and pug/plantzhub.com/routes/products.js
@@ -23,10 +23,18 @@ router.get("/delete/:id", async function(req, res, next) {
     let product = await Product.findByIdAndDelete(req.params.id);
     res.redirect("/products");
 });
-router.get("/edit", async function(req, res, next) {
-    res.render("products/edit");
+router.get("/edit/:id", async function(req, res, next) {
+    let product = await Product.findById(req.params.id);
+    res.render("products/edit", { product });
 });
 
+router.post("/edit/:id", async function(req, res, next) {
+    let product = await Product.findById(req.params.id);
+    product.name = req.body.name;
+    product.price = req.body.price;
+    await product.save();
+    res.redirect("/products");
+});
 
 
 module.exports = router;
