ea510848a1ddc55abf5fa7fb531513469fef2e3e
MATI UL REHMAN <iammatiz00@gmail.com>
2022-12-25 20:23:17 +0500

    cart completed

--- node/express and pug/plantzhub.com/routes/index.js
+++ node/express and pug/plantzhub.com/routes/index.js
@@ -7,9 +7,7 @@ router.get("/", function(req, res, next) {
     res.render("index", { title: "Express" });
 });
 
-router.get("/cart", function(req, res, next) {
-    res.render("cart");
-});
+
 router.get("/cart/:id", async function(req, res, next) {
     let product = await Product.findById(req.params.id);
     console.log("Add This Product in cart");
@@ -17,8 +15,30 @@ router.get("/cart/:id", async function(req, res, next) {
     if (req.cookies.cart) {
         cart = req.cookies.cart;
     }
+    cart.push(product);
     res.cookie("cart", cart);
     res.redirect("/products");
+});
+
+router.get("/cart", function(req, res, next) {
+    let cart = req.cookies.cart;
+    if (!cart) {
+        cart = [];
+    }
+    res.render("cart", { cart });
+});
+
+router.get("/cart/remove/:id", async function(req, res, next) {
+    let cart = [];
+    if (req.cookies.cart) {
+        cart = req.cookies.cart;
+    }
+    cart.splice(
+        cart.findIndex((c) => c._id == req.params.id),
+        1
+    );
+    res.cookie("cart", cart);
+    res.redirect("/cart");
 });
 
 
