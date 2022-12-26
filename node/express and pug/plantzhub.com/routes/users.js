ab718e69b5572817a6cb19cf1f035f44291e8295
MATI UL REHMAN <iammatiz00@gmail.com>
2022-12-26 23:56:59 +0500

    users

--- node/express and pug/plantzhub.com/routes/users.js
+++ node/express and pug/plantzhub.com/routes/users.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get("/register", function(req, res, next) {
-    res.send("respond with a resource");
+    res.render("users/register");
 });
 
 module.exports = router;
