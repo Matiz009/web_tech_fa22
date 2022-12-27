6a72a2abefdc4ee986a5db0ec388adab20c979c8
MATI UL REHMAN <iammatiz00@gmail.com>
2022-12-27 12:57:28 +0500

    get completed

--- node/restfulAPI_using_mongodb/restfulApi/routes/api/products.js
+++ node/restfulAPI_using_mongodb/restfulApi/routes/api/products.js
@@ -1,8 +1,23 @@
 const express = require("express");
 const router = express.Router();
 const Product = require("../../models/product");
+//getting multiple records
 router.get("/", async(req, res) => {
-    let product = await Product.find();
-    return res.send(product);
+    let products = await Product.find();
+    return res.send(products);
+});
+//getting single records
+
+router.get("/:id", async(req, res) => {
+    try {
+        let product = await Product.findById(req.params.id);
+        if (!product) {
+            return res.status(400).send("Record not present");
+        }
+        return res.send(product);
+    } catch (err) {
+        res.status(400).send("invalid id");
+        console.log(err);
+    }
 });
 module.exports = router;
