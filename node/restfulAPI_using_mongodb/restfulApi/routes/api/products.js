8340d2f9983a5ffa2e0cc3e55b6583d14b993a55
MATI UL REHMAN <iammatiz00@gmail.com>
2022-12-28 00:13:46 +0500

    all requests completed

--- node/restfulAPI_using_mongodb/restfulApi/routes/api/products.js
+++ node/restfulAPI_using_mongodb/restfulApi/routes/api/products.js
@@ -20,4 +20,33 @@ router.get("/:id", async(req, res) => {
         console.log(err);
     }
 });
+//update
+router.put("/:id", async(req, res) => {
+    let product = await Product.findById(req.params.id);
+    product.name = req.body.name;
+    product.price = req.body.price;
+    await product.save();
+    return res.send(product);
+});
+
+//delete
+router.delete("/:id", async(req, res) => {
+    let product = await Product.findByIdAndDelete(req.params.id);
+    return res.send(product);
+});
+
+//insert
+router.post('/', async(req, res) => {
+    try {
+        let product = new Product();
+        product.id = req.body.id;
+        product.name = req.body.name;
+        product.price = req.body.price;
+        await product.save();
+        return res.send(product);
+    } catch (error) {
+        res.status(400).send("invalid bad request!");
+        console.log(error);
+    }
+})
 module.exports = router;
